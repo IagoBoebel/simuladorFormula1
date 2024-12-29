44145ab2c3c6d15c7180b5f057b4f6a70e77070c
Iago <iagogtr@gmail.com>
2024-12-28 21:28:57 -0300

    First Commit

---
+++ src/main/java/com/org/simuladorformula1/SimuladorFormula1Application.java
@@ -0,0 +1,13 @@
+package com.org.simuladorformula1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorFormula1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimuladorFormula1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/simuladorformula1/SimuladorFormula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.simuladorformula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorFormula1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
